3d0f6c8be5d291d49571f5c1deedd57e3a6143fd
Alejandro Nieto Cuarenta <alejandronieto40@gmail.com>
2025-10-05 00:46:49 +0200

    update electronics_basic: requirement analysis

--- project_Electronics_Basics/syside_commands.py
+++ project_Electronics_Basics/syside_commands.py
@@ -1,5 +1,5 @@
-import syside_license
-syside_license.check()  # Validates your license
+# import syside_license
+# syside_license.check()  # Validates your license
 import syside
 
 model, diagnostics = syside.load_model(["project_Electronics_Basics/electronics_basic.sysml"])
